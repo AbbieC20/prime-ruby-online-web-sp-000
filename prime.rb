0e28f7ff7c39ee791b37acaf186daabfb64b322a
remote-torrent-6747 <abbyc@sky.com>
2020-12-21 17:02:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i|
-  if num % i == 0 && i < num
-    return false
+  2.upto(Math.sqrt(n)) do |x| # testing up to the square root of the number because going past there is excessive
+    if n % x == 0
+      # n is the number being called from the program
+      # x is the number we're dividing by, counting from 2 up to the square root of the number
+      return false # this means the number is not prime
+    else
+      return true # this means the number is prime
+    end
   end
-  }
-  true
 end
