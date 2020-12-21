627c045fdafee34aaea9be757776e8c8e7819a14
remote-torrent-6747 <abbyc@sky.com>
2020-12-21 16:54:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(number)
   (2..(number - 1)).each do |n|
-    return false if number % n == 0 || number.negative?(number)
+    return false if number % n == 0
   end
     return true
 end
